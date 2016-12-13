64b9eae8d06066fc08f1248ddb64731b4cb571bb
Harry Aydin <harry.aydin.23@gmail.com>
2016-12-13 14:08:22 +1100

    refactor nock tests

--- test/api.test.js
+++ test/api.test.js
@@ -1,21 +1,10 @@
 import wikipediaArticles from '../src/api'
-import nock from 'nock'
-
-const query = 'test'
-var wikipedia = nock('http://en.wikipedia.org')
-                .get('/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=15&prop=pageimages%7Cextracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=test')
-                .reply(200, {
-                  query: {
-                    pages: {
-                      1111: {},
-                      2222: {}
-                    }
-                  }
-                })
+import { nockWikiArticles } from './nocks'
 
 describe('Wikipedia search endpoint', () => {
   it('should return an array of articles', () => {
-    
+  const query = 'test'
+    nockWikiArticles(query)  
     return wikipediaArticles(query)
       .then((res) => {
         expect(res.length).toBeGreaterThan(1)
